77e9b28751703e5cc937ed44832d0d46cd163167
naaresz <andirafinnanareswari@gmail.com>
2026-04-27 12:15:46 +0700

    update: perbaiki layout auth

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,18 +1,23 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 export default function AuthLayout(){
     return(
-        <>
-        <Header />
+        <div className="grid grid-cols-2 min-h-screen">
+            {/*GAMBAR*/}
+            <div className="bg-gray-100 flex items-center justify-center min-h-screen">
+                <img 
+                    src="https://www.invofest-harkatnegeri.com/assets/nav-logo.png" 
+                    alt="" 
+                    className="max-w-sm"
+                />
+            </div>
 
-        <main className="bg-gray-100 py-10">
+        <main className="bg-gray-100 p-6">
             <Outlet/>
         </main>
 
-        <Footer />
 
-        </>
+
+        </div>
     );
 }
